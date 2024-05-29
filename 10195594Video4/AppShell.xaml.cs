aa85737c5356b5d48e50440e0d76c09baa409445
Estrella <Estrella@MELI>
2024-05-29 17:11:07 -0600

    Agregar archivos de proyecto.

---
+++ 10195594Video4/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _10195594Video4
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 10195594Video4/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace _10195594Video4
+{
+    public partial class MainPage : ContentPage
+    {
+   
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+}
